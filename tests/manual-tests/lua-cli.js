b9d07132efcadde006a07eff7bd5ad27b4a6bd2e
daurnimator <quae@daurnimator.com>
2017-04-12 19:13:39 +1000

    Try CLI input with 'return' prepended

--- tests/manual-tests/lua-cli.js
+++ tests/manual-tests/lua-cli.js
@@ -12,6 +12,8 @@ readlineSync.setDefaultOptions({
     prompt: '> '
 });
 
+const stdin = lua.to_luastring("=stdin");
+
 const L = lauxlib.luaL_newstate();
 
 linit.luaL_openlibs(L);
@@ -24,10 +26,26 @@ for (;;) {
     if (input.length === 0)
         continue;
 
-    let buffer = lua.to_luastring(input);
-    if (lauxlib.luaL_loadbuffer(L, buffer, buffer.length, lua.to_luastring("=stdin"))
-        || lapi.lua_pcall(L, 0, 0, 0)) {
+    let status;
+    {
+        let buffer = lua.to_luastring("return " + input);
+        status = lauxlib.luaL_loadbuffer(L, buffer, buffer.length, stdin);
+    }
+    if (status !== lua.thread_status.LUA_OK) {
+        lapi.lua_pop(L, 1);
+        let buffer = lua.to_luastring(input);
+        if (lauxlib.luaL_loadbuffer(L, buffer, buffer.length, stdin) === lua.thread_status.LUA_OK) {
+            status = lua.thread_status.LUA_OK;
+        }
+    }
+    if (status !== lua.thread_status.LUA_OK) {
         lauxlib.lua_writestringerror(`${lapi.lua_tojsstring(L, -1)}\n`);
+        continue;
+    }
+    if (lapi.lua_pcall(L, 0, 0, 0) !== lua.thread_status.LUA_OK) {
+        lauxlib.lua_writestringerror(`${lapi.lua_tojsstring(L, -1)}\n`);
+        lapi.lua_settop(L, 0);
+        continue;
     }
     lapi.lua_settop(L, 0);  /* remove eventual returns */
 }
