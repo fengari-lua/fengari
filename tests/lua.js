35e1ec3457c653521e8afa7cd05984309f0ba67a
Benoit Giannangeli <giann008@gmail.com>
2017-04-18 12:13:43 +0200

    tests locals.lua

--- tests/lua.js
+++ tests/lua.js
@@ -8,6 +8,34 @@ const lua      = require('../src/lua.js');
 const linit    = require('../src/linit.js');
 
 
+test('locals.lua', function (t) {
+    let luaCode = `
+        _soft = true
+        require = function(lib) return _G[lib] end  -- NYI
+        return dofile("tests/lua-tests/locals.lua")
+    `, L;
+    
+    t.plan(1);
+
+    t.doesNotThrow(function () {
+
+        L = lauxlib.luaL_newstate();
+
+        linit.luaL_openlibs(L);
+
+        lauxlib.luaL_loadstring(L, lua.to_luastring(luaCode));
+
+    }, "Lua program loaded without error");
+
+    // t.doesNotThrow(function () {
+
+        lapi.lua_call(L, 0, -1);
+
+    // }, "Lua program ran without error");
+
+});
+
+
 test('constructs.lua', function (t) {
     let luaCode = `
         _soft = true
