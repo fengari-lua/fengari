22220c5992ed660ec6ed805b4275f9ec1378f60c
daurnimator <quae@daurnimator.com>
2017-08-18 14:05:49 +1000

    webpack.config.js: Use babel-loader instead of babili-webpack-plugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,41 +1,35 @@
 const webpack = require('webpack');
 const path = require('path');
-const BabiliPlugin = require("babili-webpack-plugin");
 
 module.exports = [
     {
         entry: './src/fengari.js',
         target: 'web',
+        node: false,
         output: {
             path: path.resolve(__dirname, 'dist'),
             filename: 'fengari.js',
             library: 'fengari'
         },
-        externals: {
-            "crypto": "crypto"
+        module: {
+            rules: [
+                {
+                    test: [/\.js$/],
+                    loader: 'babel-loader',
+                    options: {
+                        presets: [['env', {
+                            "targets": {
+                                "browsers": ["last 2 versions", "safari >= 7"]
+                            }
+                        }]]
+                    }
+                }
+            ]
         },
         plugins: [
             new webpack.DefinePlugin({
                 WEB: JSON.stringify(true),
             })
-        ]
-    },
-    {
-        entry: './src/fengari.js',
-        target: 'web',
-        output: {
-            path: path.resolve(__dirname, 'dist'),
-            filename: 'fengari.min.js',
-            library: 'fengari'
-        },
-        externals: {
-            "crypto": "crypto"
-        },
-        plugins: [
-            new webpack.DefinePlugin({
-                WEB: JSON.stringify(true),
-            }),
-            new BabiliPlugin()
         ]
     }
 ];
