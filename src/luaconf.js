fengari-lua/fengari

commit c260105662e28778fe129a3a4500c1baf40ea109

src/lvm.js: Optimise lua_integer2str and lua_number2str

src/luaconf.js
@@ -1,7 +1,5 @@
 "use strict";
 
-const sprintf = require('sprintf-js').sprintf;
-
 const LUA_MAXINTEGER = 2147483647;
 const LUA_MININTEGER = -2147483648;
 
@@ -22,11 +20,11 @@ const LUAI_MAXSTACK = 100000;
 const LUA_IDSIZE = 60-1; /* fengari uses 1 less than lua as we don't embed the null byte */
 
 const lua_integer2str = function(n) {
-    return sprintf(LUA_INTEGER_FMT, n);
+    return String(n); /* should match behaviour of LUA_INTEGER_FMT */
 };
 
 const lua_number2str = function(n) {
-    return sprintf(LUA_NUMBER_FMT, n);
+    return String(Number(n.toPrecision(n))); /* should match behaviour of LUA_NUMBER_FMT */
 };
 
 const lua_numbertointeger = function(n) {

commit ca087aef0afc793e52e870da57cfddb17eeb5701

src/luaconf.js: Fix bug introduced in c260105662e28778fe129a3a4500c1baf40ea109

src/luaconf.js
@@ -24,7 +24,7 @@ const lua_integer2str = function(n) {
 };
 
 const lua_number2str = function(n) {
-    return String(Number(n.toPrecision(n))); /* should match behaviour of LUA_NUMBER_FMT */
+    return String(Number(n.toPrecision(14))); /* should match behaviour of LUA_NUMBER_FMT */
 };
 
 const lua_numbertointeger = function(n) {